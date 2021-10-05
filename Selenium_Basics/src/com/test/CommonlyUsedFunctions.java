d2dc13f526fa0b79d916aea581afd5e65ec738dd
Vivek Nadimetla <Vivek Nadimetla@LAPTOP-E63QMONL>
2021-10-06 00:08:53 +0530

    Adding WaitForElement and WaitForClickableElement functions

--- Selenium_Basics/src/com/test/CommonlyUsedFunctions.java
+++ Selenium_Basics/src/com/test/CommonlyUsedFunctions.java
@@ -1,22 +1,31 @@
 package com.test;
 
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.lang.System.Logger;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.beust.jcommander.internal.Console;
 
 public class CommonlyUsedFunctions {
 
 	public static void main(String[] args) throws InterruptedException {
-		//GetTitle();
-		//SendInput("Hello","//*[@title='Search']");
-		//FetchText("https://www.google.com","//div[@class='vcVZ7d']//a[1]");
+		GetTitle();
+		SendInput("Hello","//*[@title='Search']"); //Provide input string as first arg and xpath of the xpath of input field as second arg 
+		FetchText("https://www.google.com","//div[@class='vcVZ7d']//a[1]"); //Provide xpath of the element you want to fetch in text format
 		SetZoomToZero();
+		WaitForElement(); //Provide Xpath of the element you want Selenium to wait until it is visible
+		WaitForClickableElement(); //Provide xpath of the element you want Selenium to wait until clickable
+		
 		
 	}
 
@@ -79,19 +88,70 @@ public class CommonlyUsedFunctions {
 		 System.setProperty("webdriver.chrome.driver", "C:\\Selenium-java\\chromedriver.exe");
 		 driver = new ChromeDriver();
 		 driver.get("https://www.google.com");
+		 driver.manage().window().maximize();
+	     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		// WebElement zoom = driver.findElement(By.tagName("html"));
-		 Thread.sleep(5000);
+		 //Thread.sleep(5000);
 		 //zoom.sendKeys(Keys.chord(Keys.CONTROL, "0")); 
-		 driver.findElement(By.tagName("html")).sendKeys(Keys.chord(Keys.CONTROL, "0"));
+	     Robot robot = new Robot();
+	     System.out.println("About to zoom in");
+	     for (int i = 0; i < 3; i++) 
+	     {			
+			robot.keyPress(KeyEvent.VK_CONTROL);
+			robot.keyPress(KeyEvent.VK_ADD);
+			robot.keyRelease(KeyEvent.VK_ADD);
+			robot.keyRelease(KeyEvent.VK_CONTROL);
+		}
+	     Thread.sleep(5000);
+	     robot.keyPress(KeyEvent.VK_CONTROL);
+	     robot.keyPress(KeyEvent.VK_0);
+	     Thread.sleep(5000);
+	     robot.keyRelease(KeyEvent.VK_CONTROL);
+	     robot.keyRelease(KeyEvent.VK_0);
 		 driver.close();
 		 driver.quit();
-			 
-		 }//test
+		 Thread.sleep(5000);
+		 }
 		 catch (Exception e) {
 			System.out.println(e);
 		}
 
 	 }
+	 
+	 //Explicit wait until a specific element is visible
+	 public static void WaitForElement(String... args) {
+		 try {
+			 WebDriver driver;
+			 System.setProperty("webdriver.chrome.driver", "C:\\Selenium-java\\chromedriver.exe");
+			 driver = new ChromeDriver();
+			 driver.get("https://www.google.com");
+			 driver.manage().window().maximize();
+			 WebDriverWait wait = new WebDriverWait(driver, 10);
+			 wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(args[0])));
+			 System.out.println("Expected element is visible");
+		 }
+		 catch (Exception e) {
+			 System.out.println(e);
+		 }
+	 }
+	 
+	 //Explicit wait until a specific element is clickable
+	 public static void WaitForClickableElement(String... args) {
+		 try {
+			 WebDriver driver;
+			 System.setProperty("webdriver.chrome.driver", "C:\\Selenium-java\\chromedriver.exe");
+			 driver = new ChromeDriver();
+			 driver.get("https://www.google.com");
+			 driver.manage().window().maximize();
+			 WebDriverWait wait = new WebDriverWait(driver, 10);
+			 wait.until(ExpectedConditions.elementToBeClickable(By.xpath(args[0])));
+			 System.out.println("Expected clickalble element is visible");
+		 }
+		 catch (Exception e) {
+			 System.out.println(e);
+		}
+	 }
+	 
 
 
 
